8081edd6bb16525f6b612b629ae87184da4bf5f3
Valeriy Kharitonov <Valeriy.Kharitonov@dataart.com>
2015-08-05 15:31:48 +0300

    No bufferization in ConnectionManager

--- server/src/main/java/com/dataart/vkharitonov/practicechat/server/net/ConnectionManager.java
+++ server/src/main/java/com/dataart/vkharitonov/practicechat/server/net/ConnectionManager.java
@@ -9,9 +9,9 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 import org.apache.commons.net.io.Util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
@@ -55,9 +55,11 @@ public final class ConnectionManager {
      * Stops listening to connections.
      */
     public void stop() {
-        workerThread.interrupt();
-        Util.closeQuietly(server);
-        executor.shutdown();
+        if (workerThread != null) {
+            workerThread.interrupt();
+            Util.closeQuietly(server);
+            executor.shutdown();
+        }
     }
 
     private class WorkerThread extends Thread {
@@ -94,7 +96,7 @@ public final class ConnectionManager {
         }
 
         private Message parseMessage(Socket client) throws IOException {
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+            Reader in = new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8);
             JsonReader reader = new JsonReader(in);
             return JsonUtils.GSON.fromJson(reader, Message.class);
         }
